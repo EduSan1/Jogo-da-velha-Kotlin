b0255e20aacaf12acbc67693ac62575ca5350f55
EDUARDO SANTOS NASCIMENTO <21276323@CFP127ALUNO.LOCAL>
2022-03-15 16:44:52 -0300

    1.0 - tela home e dificuldade pronta

---
+++ app/src/main/java/br/senai/sp/jogodaveia/dificuldadeActivity.kt
@@ -0,0 +1,11 @@
+package br.senai.sp.jogodaveia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class dificuldadeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dificuldade)
+    }
+}
